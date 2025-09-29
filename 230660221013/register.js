da3453f1472945f4b70f987bb22a26254dbb7ce1
MuhammadAuliaRamadhani230660221013 <230660221013@student.unsap.ac.id>
2025-09-29 15:38:21 +0700

    Membuat Direktori 230660221013

---
+++ 230660221013/register.js
@@ -0,0 +1,56 @@
+// register.js
+
+// --- INPUT DATA ---
+const firstName = "Muhammad";
+const lastName = "Aulia Ramadhani";
+const nik = "230660221013";
+const position = "Backend Developer";
+let grossSalary = 9500000;
+let isPermanent = true;
+
+
+// --- PROSES DATA ---
+
+// 1. Ambil inisial nama
+const initialFirstName = firstName.slice(0, 1); // Hasil: "B"
+const initialLastName = lastName.slice(0, 1);   // Hasil: "P"
+
+// 2. Ambil 4 digit terakhir NIK
+const lastNikDigits = nik.slice(-4); // Hasil: "0003"
+
+// 3. Gabungkan semuanya dan ubah ke huruf besar
+const employeeID = (initialFirstName + initialLastName + lastNikDigits).toUpperCase(); // Hasil: "BP0003"
+
+// 4. Format Gaji ke format Rupiah
+// toLocaleString('id-ID') adalah cara bawaan JavaScript untuk format angka Indonesia
+
+const formattedSalary = `Rp ${grossSalary.toLocaleString('id-ID')}`; // Hasil: "Rp 9.500.000"
+
+// 5. Ubah boolean menjadi teks yang deskriptif
+
+const statusText = isPermanent ? "Karyawan Tetap" : "Karyawan Kontrak"; // Hasil: "Karyawan Tetap"
+// Ini menggunakan operator ternary, versi singkat dari if-else.
+
+// --- OUTPUT ---
+
+
+const summary = `https://github.com/MuhammadAuliaRamadhani230660221013/SI-VA.git
+===================================================
+        DATA PENDAFTARAN KARYAWAN BARU
+===================================================
+
+SELAMAT DATANG DI PT. CIPTA SOLUSI DIGITAL!
+
+Berikut adalah ringkasan data Anda:
+
+  Nama Lengkap      : ${(firstName + " " + lastName).toUpperCase()}
+  Posisi            : ${position}
+  Employee ID       : ${employeeID}
+  
+  Gaji Pokok (Gross): ${formattedSalary}
+  Status            : ${statusText}
+
+===================================================
+`;
+
+console.log(summary);
