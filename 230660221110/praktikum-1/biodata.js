873ece51b87d65af9814e7021123e892f9051901
komarun19 <komarudinar968@gmail.com>
2025-10-20 11:33:56 +0700

    menyelesaikan study case dan praktek-4

--- 230660221110/praktikum-1/biodata.js
+++ 230660221110/praktikum-1/biodata.js
@@ -1,17 +1,10 @@
 // biodata.js
 
 // 1. Deklarasikan variabel menggunakan const dan let
-<<<<<<<< HEAD:230660221110/praktikum-1/biodata.js
-const namaLengkap = "Komarudin AR"; // Ganti dengan nama lengkapmu
+const namaLengkap = "Komarudin"; // Ganti dengan nama lengkapmu
 const tempatLahir = "Sumedang";
 let tanggalLahir = "19 September 2002"; // Format bisa disesuaikan
-let umur = 25; // Ganti dengan umurmu
-========
-const namaLengkap = "Dede Dian Permana"; // Ganti dengan nama lengkapmu
-const tempatLahir = "Sumedang";
-let tanggalLahir = "01 Mei 2005"; // Format bisa disesuaikan
 let umur = 21; // Ganti dengan umurmu
->>>>>>>> ffa7486e79eee2732da497bd327230a277d8025f:Praktikum-1/biodata.js
 let statusMahasiswa = true; // Gunakan boolean (true jika aktif, false jika tidak)
 
 // 2. Gunakan string methods untuk memanipulasi data
